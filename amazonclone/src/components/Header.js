a374e90583f5b8a0765bbc0b44c87a9db863604f
Haris Ahmad <haris18896@gmail.com>
2021-07-03 11:04:31 -0700

    Header completed, Now working on Home Screen

--- amazonclone/src/components/Header.js
+++ amazonclone/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
 
 function Header() {
     return (
@@ -33,6 +35,11 @@ function Header() {
                 <div className="header__option">
                     <span className="header__optionLineOne">your</span>
                     <span className="header__optionLineTwo">Prime</span>
+                </div>
+
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionLineTwo header__basketCount">0</span>
                 </div>
 
             </div>
